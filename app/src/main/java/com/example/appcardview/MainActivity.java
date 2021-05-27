e3aa0833f4ac5142c78a190541d32fdfd8c3ddfd
Deepam Naik <deepamnaik14@gmail.com>
2021-05-27 14:21:33 +0400

    Creating recycler view

--- app/src/main/java/com/example/appcardview/MainActivity.java
+++ app/src/main/java/com/example/appcardview/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.appcardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    List<DataClass> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        list.add(new DataClass("Deepam", "123456789", R.drawable.donut));
+        list.add(new DataClass("idk", "412358456", R.drawable.cat));
+        list.add(new DataClass("sum1", "452146258", R.drawable.banana));
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
